b0e0e1264e0655aff172ab7dcbbbd624ab455f07
AnEvilBrit <Ssaunders20205@gmail.com>
2023-12-05 05:34:37 +0000

    Hopefully it works

--- js/github.js
+++ js/github.js
@@ -1,16 +1,22 @@
+/**
+ * See https://developer.github.com/v3/repos/branches/#get-branch
+ *
+ * Example Github api request:
+ * https://api.github.com/repos/ta-dachi/eatsleepcode.tech/branches/master
+ */
 class LatestCommitComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: "",
+      date : "",
     };
   }
-
+  
   componentDidMount() {
     // Replace this with your own repo
     // https://api.github.com/repos/:owner/:repo/branches/master
     fetch(
-      "https://api.github.com/repos/AnEvilBrit/anevilbrit.github.io/branches/master"
+      "https://api.github.com/repos/AnEvilBrit/anevilbrit.github.io/branches/main"
     )
       .then(response => {
         response.json().then(json => {
@@ -27,9 +33,11 @@ class LatestCommitComponent extends React.Component {
 
   render() {
     return (
-        <div>{this.state.date}</div>
+      <div>
+        Here you can view all the small projects and items I have created. (Updated as of <time class="timeago" datetime={this.state.date}>Time Not Loaded!</time>)
+      </div>
     );
   }
 }
 
-ReactDOM.render(<LatestCommitComponent />, document.getElementById("root"));
+ReactDOM.render(<LatestCommitComponent />, document.getElementById("time"));
