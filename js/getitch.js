651b31402fcadd658ccf23f4b608256487c75bcd
AnEvilBrit <77389973+AnEvilBrit@users.noreply.github.com>
2024-04-03 22:53:02 +0100

    Made the html code better
    removed the reused code, and made a fuction

--- js/getitch.js
+++ js/getitch.js
@@ -1,15 +1,20 @@
-function userGame(user, game) {}
+var username, gamename;
 
-Itch.getGameData({
-    user: this.user,
-    game: this.game,
-    onComplete: function(data) {
-        var b = document.getElementById("buy_button")
-        b.href = data.links.self
-        var p = document.getElementById("price")
-        p.innerHTML = data.price
-        var i = document.getElementById("itch_icon")
-        i.src = data.cover_image
-        console.log(data)
-    }
-})
+function userGame(user, game) {
+    username = user;
+    gamename = game;
+
+    Itch.getGameData({
+        user: username,
+        game: gamename,
+        onComplete: function(data) {
+            var b = document.getElementById("buy_button")
+            b.href = data.links.self
+            var p = document.getElementById("price")
+            p.innerHTML = data.price
+            var i = document.getElementById("itch_icon")
+            i.src = data.cover_image
+            console.log(data)
+        }
+    })
+}
