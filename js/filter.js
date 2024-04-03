68dc287a4088054d6b43f6664bab482cfb8af76f
AnEvilBrit <77389973+AnEvilBrit@users.noreply.github.com>
2024-04-03 21:43:32 +0100

    Update filter.js

--- js/filter.js
+++ js/filter.js
@@ -4,7 +4,7 @@ var filter = [];
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
-  if (c === "all") c = "";
+  if (c == "all") c = "";
   for (i = 0; i < x.length; i++) {
     RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
@@ -16,7 +16,7 @@ function AddClass(element, name) {
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) === -1) {element.className += " " + arr2[i];}
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
   }
 }
 
