092658fcb75b25c7495771d4c4108c46a94d9a6b
AnEvilBrit <77389973+AnEvilBrit@users.noreply.github.com>
2024-04-07 15:07:21 +0100

    Added two types of files

--- Code/Test.cpp
+++ Code/Test.cpp
@@ -1,50 +1,57 @@
-// Sets default values for this actor's properties
-ALightSwitch();
+// Fill out your copyright notice in the Description page of Project Settings.
 
-//Vector2D Variable
-UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "First|Second|Third")
-FVector2D Vector2;
+#include "LightSwitch.h"
 
-//Vector3 Variable
-UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "First|Second")
-FVector Vector3;
-
-//Vector4 Variable
-UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "First")
-FVector4 Vector4;
-
-//Float Variable
-UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "First|Second|Third")
-float Float = 1.6f;
-
-UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "First")
-FColor Colour;
-
-UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "First")
-AActor* Actor = nullptr;
-
-UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "First")
-ACPPProjectile* Projectile = nullptr;
-
-UPROPERTY(EditAnywhere,	BlueprintReadWrite, Category = "Second", Meta =(AllowedClasses = "LightSwitch, CPPProjectile"))
-TArray&lt;AActor*> Actors;
-
-UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Second", Meta = (ClampMin = "1", ClampMax = "8"))
-TMap&lt;int32, FString> ActorMap;
-
-UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Second")
-TSet&lt;int32> ActorSet;
-
-UFUNCTION(BlueprintCallable, Category = "First")
-bool Function(bool bInput) {
-// Set the value of bIsEnabled to the input value
-bIsEnabled = bInput;
-
-// Return the new value of bIsEnabled
-return bIsEnabled;
+// Sets default values
+ALightSwitch::ALightSwitch()
+{
+	// Set this actor to call Tick() every frame
+	PrimaryActorTick.bCanEverTick = true;
 }
 
-if (!bIsEnabled) {
-// If bIsEnabled is false, return
-return;
+// Called when the game starts or when spawned
+void ALightSwitch::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Switch statement
+	switch (SwitchInput) {
+		case 1:
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::White, TEXT("Case 1"));
+			}
+			Function(true);
+			break;
+		
+		case 2:
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::White, TEXT("Case 2"));
+			}
+			break;
+		
+		default:
+			break;
+	}
+
+	//If statement
+	if (bIsEnabled == true) {
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("True"));
+		}
+	} else {
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("False"));
+		}
+	}
+}
+
+// Called every frame
+void ALightSwitch::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
 }
